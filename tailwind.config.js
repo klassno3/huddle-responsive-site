5a6216161109ec237bdb9b605db864234d5625ee
betelhem <betelhemkirub@gmail.com>
2023-01-03 20:38:26 +0300

    huddle page

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      dt: "930px",
+      mb: "375px",
+    },
+    extend: {
+      fontFamily: {
+        pop: ["Poppins"],
+        osans: ["Open Sans"],
+      },
+      colors: {
+        Pink: "hsl(322, 100%, 66%)",
+        LightPink: "hsl(321, 100%, 78%)",
+        LightRed: "hsl(0, 100%, 63%)",
+        grey: "rgba(78, 78, 78, 0)",
+
+        VeryDarkCyan: "hsl(192, 100%, 9%)",
+        VeryPaleBlue: "hsl(207, 100%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
